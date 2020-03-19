3a15feecde9d3d28569187dcb8de8e225a09932d
Patrick <patrick@willetts.dev>
2020-03-18 20:26:05 -0400

    add stitch functions for app client

--- src/lib/StitchFunctions.js
+++ src/lib/StitchFunctions.js
@@ -1,2 +1,22 @@
 import {Stitch} from 'mongodb-stitch-browser-sdk'
 
+function initalizeStitchClient(){
+    try {
+        return Stitch.initializeDefaultAppClient('stayneighbor-bjuma')
+    } catch(e){
+        return Stitch.defaultAppClient
+    }
+}
+
+function establishStitchConnection(){
+    //get default app client
+    const client = initalizeStitchClient()
+
+    if(client.errorCode) return {errorCode: '002', errorMessage: client}
+
+    const mongodb = initalizeStitchClient(client)
+
+    if(mongodb.errorCode) return {errorCode: '003', errorMessage: mongodb}
+
+    return mongodb
+}
